e923cedbe14a69b0f9a343d331621451192e222a
David Raymond <david.raymond@cdbdx.biz>
2021-10-12 16:46:53 +0200

    feat: add left/right props on Layout

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import "./default.css";
 import Layout from "./Layout";
 
 const body = document.getElementsByTagName("body")[0];
-const layout = new Layout(body);
+
+const left = document.createTextNode("LEFT");
+const right = document.createTextNode("RIGHT");
+
+const layout = new Layout(body, { left, right });
 
 layout.render();
