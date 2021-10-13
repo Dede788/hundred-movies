05b6e2f0c6ce18e047a00264e81b9d121681e99e
David Raymond <david.raymond@cdbdx.biz>
2021-10-13 12:33:38 +0200

    feat: 🎸 Add search input

--- src/SearchScreen/index.js
+++ src/SearchScreen/index.js
@@ -1,9 +1,20 @@
+import "../theme.css";
+import "./styles.css";
+
+import { doc } from "prettier";
 import Screen from "../Screen";
 
 export default class SearchScreen extends Screen {
   constructor() {
+    const content = document.createElement("div");
+
     const searchInput = document.createElement("input");
+    searchInput.setAttribute("type", "text");
     searchInput.setAttribute("placeholder", "Enter a movie title");
-    return super({ title: "Search", children: searchInput });
+    searchInput.classList.add("search");
+
+    content.appendChild(searchInput);
+
+    return super({ title: "Search", children: content });
   }
 }
